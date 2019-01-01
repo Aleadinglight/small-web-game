1ba2565840fdf5fa90ba2bfc28f6980eab8a5e3a
unknown <huutam.ptnk@gmail.com>
2019-01-01 04:54:54 +0200

    added addforce for player but it is so funny =))))))

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	public float upForce = 200f;
-	public float leftForce = 200f;
-	public float rightForce = 200f;
+	public float upForce = 50f;
+	public float leftForce = -50f;
+	public float rightForce = 50f;
 	
 	private bool isDead = false;
-	private Rigidbody2d rb2d;
+	private Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Start () {
-		rb2d = GetComponent <Rigidbody2d> ();
+		rb2d = GetComponent <Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (isDead == false){
 			if (Input.GetKey("up")){
-				rb2d..velocity = Vector2.zero;
+				rb2d.velocity = Vector2.zero;
 				rb2d.AddForce(new Vector2(0, upForce));
 			}
 			if (Input.GetKey("left")){
-				rb2d..velocity = Vector2.zero;
-				rb2d.AddForce(new Vector2(0, leftForce));
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce(new Vector2(leftForce, 0));
 			}
 			if (Input.GetKey("right")){
-				rb2d..velocity = Vector2.zero;
-				rb2d.AddForce(new Vector2(0, rightForce));
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce(new Vector2(rightForce, 0));
 			}
 		}
 	}
