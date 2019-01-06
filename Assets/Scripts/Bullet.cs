f552d096faa58a3b685097f1f298ec09b6b32a0b
unknown <huutam.ptnk@gmail.com>
2019-01-06 23:15:27 +0200

    added bullet collide on Enemy

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
+	public float speed = 0.8f;
+	private Rigidbody2D rb2d;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb2d = GetComponent <Rigidbody2D> ();
+		rb2d.velocity = transform.right * speed;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnTringgerEnter2D(Collider2D collision){
+		if (collision.gameObject.tag=="Enemy"){
+			Enemy enemy = collision.GetComponent<Enemy>();
+			if (enemy!=null){
+				enemy.TakeDamage;
+			}
+		}
+		Destroy(gameObject);
 	}
 }
